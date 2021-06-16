43e67b56e57a68bfada24ae9d834f6a5c87fbe54
Vasilena Georgieva <75674763+vassiyanakiev@users.noreply.github.com>
2021-06-16 15:23:46 +0300

    RexExr

---
+++ RegExr/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        var text = Console.ReadLine();
+
+        var patten = new Regex(@"\b([A-Z][a-z]+) ([A-Z][a-z]+)");
+
+        MatchCollection mathe = patten.Matches(text);
+
+
+        foreach (Match item in mathe)
+        {
+            Console.Write("{0} ", string.Join(" ", item));
+        }
+
+    }
+}
---
+++ RegularExpressions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Regex regex = new Regex(@"([*@])([A-Z][a-z]{2,})\1: \[(\w)]\|\[(\w)]\|\[(\w)]\|$"); 
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                Match match = regex.Match(input);
+
+                if (match.Success)
+                {
+
+                    string title = match.Groups[2].Value;
+                    char firstLetter = char.Parse(match.Groups[3].Value);
+                    char secondLetter = char.Parse(match.Groups[4].Value);
+                    char thirdLetter = char.Parse(match.Groups[5].Value);
+
+                    Console.WriteLine($"{title}: {(int)firstLetter} {(int)secondLetter} {(int)thirdLetter}");
+
+                }
+                else
+                {
+                    Console.WriteLine("Valid message not found!");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
